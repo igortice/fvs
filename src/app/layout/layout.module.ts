0e8211c4d2d408c37df55db4cefdf7fd2b06c594
Igor Rocha <igortice@gmail.com>
2018-11-26 15:43:50 -0300

    init layout

---
+++ src/app/layout/layout.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { LayoutRoutingModule } from './layout-routing.module';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainComponent } from './main/main.component';
+
+@NgModule({
+  declarations: [ HeaderComponent, FooterComponent, MainComponent ],
+  imports:      [
+    CommonModule,
+    LayoutRoutingModule
+  ]
+})
+export class LayoutModule {}
