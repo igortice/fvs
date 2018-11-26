3699d1a064e26aefdea0da8e69a8800e390fcd80
Igor Rocha <igortice@gmail.com>
2018-11-26 16:24:41 -0300

    init routes config

--- src/app/layout/layout-routing.module.ts
+++ src/app/layout/layout-routing.module.ts
@@ -6,7 +6,9 @@ const routes: Routes = [
   {
     path:      '',
     component: LayoutComponent,
-    children:  []
+    children:  [
+      { path: '', loadChildren: '../pages/index/index.module#IndexModule' }
+    ]
   }
 ];
 
